10e2b28ec40df1fd0716f363d1a9556d3a5da07e
Manechu <manupavioni@gmail.com>
2023-02-08 19:01:26 -0300

    modulo 3 completado

---
+++ M03 JavaScript Fundamentos/jeje.js
@@ -0,0 +1,12 @@
+console.log(3 % 2 == 1)
+
+
+function soniguales (y, x)
+{if(y == x) {return true} else {return false}
+}
+
+
+
+function esVocal(letra) {if(letra = 'a','e','i','o','u'){
+    return('Es vocal')} else {return("Dato incorrecto")}
+}
